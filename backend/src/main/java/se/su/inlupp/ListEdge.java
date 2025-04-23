3304eafd9d26c4251d659a72924bb3e2e72dc8a3
EmmaC521 <emmacarlssonn@hotmail.com>
2025-04-23 10:46:33 +0200

    New commit

---
+++ backend/src/main/java/se/su/inlupp/ListEdge.java
@@ -0,0 +1,39 @@
+package se.su.inlupp;
+
+public class ListEdge<T> implements Edge<T> {
+
+    private final T destination;
+    private final String name;
+    private int weight;
+
+    public ListEdge(T destination, String name, int weight) {
+        if(weight < 0) throw new IllegalArgumentException("Vikten får inte vara negativ");
+        this.destination = destination;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public T getDestination() {
+        return destination;
+    }
+
+    @Override
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void setWeight(int weight) {
+        if(weight < 0) throw new IllegalArgumentException("Vikten får inte vara negativ");
+        this.weight = weight;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "Destination: " + destination + " Namn: " + name + " Vikt: " + weight;
+    }
+}
